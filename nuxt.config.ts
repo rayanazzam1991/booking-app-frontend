df0d59582fd2e6936d0e1e45a641e42c9267d8bb
rayanazzam <razzam@chip.com.lb>
2025-11-27 13:43:40 +0200

    Setup tailwind and ShadCN ui lib.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,43 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr:true,
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  css:['./app/assets/css/main.css'],
+  modules: [
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@vee-validate/nuxt',
+    'shadcn-nuxt',
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+  runtimeConfig: {
+    // Only available on the server (good for secrets)
+    openaiApiKey: process.env.OPENAI_API_KEY,
+
+    // Public runtime config (exposed to client)
+    public: {
+      // you can put a default system prompt here if you want
+      defaultSystemPrompt: ''
+    }
+  },
 })
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+// tailwind.config.js
+export default {
+    content: [
+        "./app/**/*.{vue,js,ts}",
+        "./components/**/*.{vue,js,ts}",
+        "./pages/**/*.{vue,js,ts}",
+        "./layouts/**/*.{vue,js,ts}",
+    ],
+}
